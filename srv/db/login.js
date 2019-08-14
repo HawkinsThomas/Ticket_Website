b50584fe7cc2e62698358324bd4eef1f2f89ba1a
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-08-14 17:04:59 -0400

    created a file for handling a db login check

---
+++ srv/db/login.js
@@ -0,0 +1,23 @@
+module.exports = {
+  login: (credentials) => {
+    query = `SELECT password FROM user WHERE username=${credentials.username}`;
+    return new Promise((resolve, reject) => {
+      db.query(query, (err, result) => {
+        if (err) {
+          reject(new Error('query error'));
+        } else {
+          const tableHeaders = Object.keys(result[0]);
+          const tableData = result.map(row => Object.values(row));
+          resolve(
+            {
+              tableHeaders,
+              tableData,
+            },
+          );
+        }
+      });
+    });
+  },
+};
+
+}
