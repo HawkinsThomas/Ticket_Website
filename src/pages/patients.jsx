6007bda43dfbc566c10aa37135bd1688570f894c
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-08-14 15:50:43 -0400

    added /srv/db folder for database operations, deleted patients page.

--- src/pages/patients.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Menu from 'components/Menu';
-import 'css/bootstrap.min.css';
-
-ReactDOM.render(<Menu />, document.getElementById('menu'));
